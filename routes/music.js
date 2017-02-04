9924a1ae8dcd07dc02e725ead4dbf1795d9a578b
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2017-02-04 16:26:20 -0500

    commit on 2-4-17 before fixing broken images

--- routes/music.js
+++ routes/music.js
@@ -15,10 +15,10 @@ router.post('/apikey', function(req, res, next){
   console.log(req.body);
   artist = req.body.search;
 
-  request.get({ url: "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" + artist + "&page_size=100&apikey=" + process.env.MUSIXMATCH_API_KEY }, function(error, response, body) { 
-      if (!error && response.statusCode == 200) { 
+  request.get({ url: "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" + artist + "&page_size=100&apikey=" + process.env.MUSIXMATCH_API_KEY }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
         data = JSON.parse(body);
-        res.send( {data: data} );      
+        res.send( {data: data} );
       };
   });
 });
@@ -29,12 +29,12 @@ router.post('/titles', function(req, res, next) {
   artistArray = req.body.artistSearch.split(' ');
   artist = artistArray.join('+');
 
-  request.get({ url: "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" + artist + "&page_size=100&apikey=45f030feddac66fcfa2f9e9f659608c4" }, function(error, response, body) { 
-      if (!error && response.statusCode == 200) { 
+  request.get({ url: "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" + artist + "&page_size=100&apikey=45f030feddac66fcfa2f9e9f659608c4" }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
         tracksArray = JSON.parse(body).message.body.track_list;
 
         pic = tracksArray[0].track.album_coverart_100x100
-        artistPic = (pic == "http://s.mxmcdn.net/images/albums/nocover.png") ? tracksArray[1].track.album_coverart_100x100 : tracksArray[0].track.album_coverart_100x100
+        artistPic = (pic == "https://s.mxmcdn.net/images/albums/nocover.png") ? tracksArray[1].track.album_coverart_100x100 : tracksArray[0].track.album_coverart_100x100
 
         titlesString = '';
 
@@ -42,7 +42,7 @@ router.post('/titles', function(req, res, next) {
           titlesString += ' ' + val.track.track_name;
         });
 
-        function titlesStringArray(string){    
+        function titlesStringArray(string){
           stringArray = titlesString.replace("'", "").replace(/,/g, "").replace(/'/g, "").replace(/\W/g, ' ').split(' ');
           return stringArray;
         };
@@ -50,20 +50,20 @@ router.post('/titles', function(req, res, next) {
         var songTitlesArray = titlesStringArray(titlesString);
 
         sentiResults = analyze("marc");
-        console.log(sentiResults); 
+        console.log(sentiResults);
 
-        res.render('titles', { 
-          lyrics: songTitlesArray, 
-          artist: artist, 
+        res.render('titles', {
+          lyrics: songTitlesArray,
+          artist: artist,
           pic: artistPic,
           posWordsString: ["my", "you", "i", "love", "feel"],
           posScore: "1",
-          negWordsString: ["hate", "no", "crazy"], 
+          negWordsString: ["hate", "no", "crazy"],
           negScore: "-10",
           overallScore: "-9"
         });
-      } 
-  }); 
+      }
+  });
 });
 
 router.post('/lyrics', function(req, res, next) {
@@ -74,12 +74,12 @@ router.post('/lyrics', function(req, res, next) {
   song = songArray.join('+');
   artistImage = req.body.artistImage;
 
-  url = "http://www.songlyrics.com/" + artist + "/" + song + "-lyrics/"
-  
+  url = "https://www.songlyrics.com/" + artist + "/" + song + "-lyrics/"
+
   x(url, '#songLyricsDiv')(function(err, lyrics) {
 
     sentiResults = analyze(lyrics);
-    console.log(sentiResults.positive.words);    
+    console.log(sentiResults.positive.words);
     songLyricsArray = lyrics.replace("'", "").replace(/,/g, "").replace(/'/g, "").replace(/\W/g, ' ').split(' ');
 
 
@@ -88,12 +88,12 @@ router.post('/lyrics', function(req, res, next) {
       res.render('form', { error: "error" });
     } else {
       res.render('lyrics', {
-        lyrics: songLyricsArray, 
-        artist: artist, 
-        song: song, 
-        posWordsString: sentiResults.positive.words, 
+        lyrics: songLyricsArray,
+        artist: artist,
+        song: song,
+        posWordsString: sentiResults.positive.words,
         posScore: sentiResults.positive.score,
-        negWordsString: sentiResults.negative.words, 
+        negWordsString: sentiResults.negative.words,
         negScore: sentiResults.negative.score,
         overallScore: sentiResults.score,
         url: url,
